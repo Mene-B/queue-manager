265e56fca4500eae8fa2f1898b6fa8fa29239811
Edouard Meneghetti <a33617@MacBook-Pro.local>
2023-07-31 03:20:22 +0200

    ajout de toues les mdoifs demandées

--- main.js
+++ main.js
@@ -17,12 +17,22 @@ const adminId = config.adminRoleId;
 const reactions = ["1️⃣","2️⃣","3️⃣","4️⃣","5️⃣","6️⃣","7️⃣","8️⃣","9️⃣"];
 client.login(config.token);
 
-client.on("ready",()=>{
+client.on("ready",async()=>{
     console.log("Bot ready to work !");
+    setInterval(async()=>{
+        const now = Date.now();
+        const lastTickets = (await clientPG.query("select * from time_out")).rows;
+        lastTickets.forEach(async(ticket)=>{
+            if (now - +(ticket.last_time) >= 3600000){
+                await clientPG.query("delete from tickets where category = $1",[ticket.category]);
+                await clientPG.query("delete from time_out where category = $1",[ticket.category])
+            }
+            return
+        })
+    },30000)
 });
 
 client.on("messageCreate", async(message)=>{
-    console.log(message);
     if(message.author.bot){
         return;
     }
@@ -37,23 +47,28 @@ client.on("messageCreate", async(message)=>{
             message.react("❌");
             return;
         }
-        await clientPG.query('insert into tickets values ($1,$2)',[message.author.id, category]);
+        await clientPG.query('insert into tickets values ($1,$2,$3)',[message.author.id, category,Date.now()]);
+        await clientPG.query("delete from time_out where category = $1",[category]);
+        await clientPG.query("insert into time_out values ($1,$2)",[category, Date.now()])
         message.react("⌛");
         return;
     }
     if((message.content === prefix+"f") || (message.content === prefix+"v") || (message.content === prefix+"a")){
         const category = message.content.split("")[1];
+        const name = (category === "f")? "Faille" : (category === "v")? "Valorant" : "Aram";
         const datas = (await clientPG.query('select * from tickets where category = $1',[category]))?.rows;
         if(datas.length === 0){
             message.reply("Il n'y a actuellement personne dans cette queue");
             return;
         }
         const embedText = datas.slice(0,9).map((element, index)=>{
-            return `${(index+1).toString()} - <@${element.user_id}>`
+            const hours = ((new Date(+(element.time))).getHours().toString().length === 2)? (new Date(+(element.time))).getHours().toString() : "0"+(new Date(+(element.time))).getHours().toString();
+            const minutes = ((new Date(+(element.time))).getMinutes().toString().length === 2)? (new Date(+(element.time))).getMinutes().toString() : "0"+(new Date(+(element.time))).getMinutes().toString();;
+            return `${(index+1).toString()} - <@${element.user_id}> | ${hours}:${minutes}`
         }).join("\n");
 
         const embed = new Discord.EmbedBuilder()
-        .setTitle("Queue " + category)
+        .setTitle("Queue " + name)
         .setDescription(embedText)
         .setColor("Yellow")
 
@@ -87,9 +102,10 @@ client.on("messageReactionAdd", async(reaction, user)=>{
         return;
     }
     await reaction.message.reactions.removeAll();
-    const category = reaction.message.embeds[0].title.split(" ")[1];
+    const category = reaction.message.embeds[0].title.split(" ")[1][0].toLowerCase();
+    const name = (category === "f")? "Faille" : (category === "v")? "Valorant" : "Aram";
     const index = reactions.indexOf(reaction.emoji.name);
-    const user_id = reaction.message.embeds[0].description.split("\n")[index].slice(6, -1);
+    const user_id = reaction.message.embeds[0].description.split("\n")[index].match(/<@([0-9]+)>/g)[0].slice(2,-1);
     await clientPG.query("delete from tickets where user_id = $1 and category = $2",[user_id, category]);
     const datas = (await clientPG.query('select * from tickets where category = $1',[category]))?.rows;
 
@@ -100,11 +116,12 @@ client.on("messageReactionAdd", async(reaction, user)=>{
     }
 
     const embedText = datas.slice(0,9).map((element, index)=>{
-        return `${(index+1).toString()} - <@${element.user_id}>`
-    }).join("\n");
+        const hours = ((new Date(+(element.time))).getHours().toString().length === 2)? (new Date(+(element.time))).getHours().toString() : "0"+(new Date(+(element.time))).getHours().toString();
+        const minutes = ((new Date(+(element.time))).getMinutes().toString().length === 2)? (new Date(+(element.time))).getMinutes().toString() : "0"+(new Date(+(element.time))).getMinutes().toString();;
+        return `${(index+1).toString()} - <@${element.user_id}> | ${hours}:${minutes}`}).join("\n");
 
     const embed = new Discord.EmbedBuilder()
-    .setTitle("Queue " + category)
+    .setTitle("Queue " + name)
     .setDescription(embedText)
     .setColor("Yellow")
 
